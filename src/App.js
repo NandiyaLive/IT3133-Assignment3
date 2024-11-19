c077240a915f411d14a27759cf53ebdaf58fae58
Neranjana Prasad <30690726+NandiyaLive@users.noreply.github.com>
2024-11-19 09:15:26 +0000

    feat: create profile component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./App.css";
 import "./assets/css/compo.css";
 import Table from "./components/table";
 import { students } from "./assets/studentsbd";
+import { useState } from "react";
+import Profile from "./components/profile";
 
 function App() {
+  const [student, setStudent] = useState(students[0]);
+
   return (
     <div className="outerDiv">
       <div className="leftDiv">
         <Table students={students} />
+      </div>
+
+      <div className="rightDiv">
+        <Profile student={student}/>
       </div>
     </div>
   );
